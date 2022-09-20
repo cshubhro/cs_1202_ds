bfcad2c7c9264d1c4fe2072b15d92bf62108b848
shubhro <shubhrogupta117@gmail.com>
2022-09-21 00:09:05 +0530

    added a function file

---
+++ functions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void arrPrinter(int arr[], int n){
+	for(int i=0; i<n; i++){
+		printf("%d ", arr[i]);
+	}
+}
+
+int main(void) {
+	int negnum = -17/3;
+	printf("%d ", negnum);
+	return 0;
+}
